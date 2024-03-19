a9fd651c1ff4ef8134e0d3b0086d6d3eb3d4afab
SiSimo9750 <bourhim.mo8@gmail.com>
2024-03-19 16:03:25 -0700

    Debugging

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,8 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t le = 0, ri = 0;
 
-		le = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		ri = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		le = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		ri = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
 		return ((le > ri) ? le : ri);
 	}
